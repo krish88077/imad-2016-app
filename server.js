13b4a26fc7b4850f7efaf96988ce3d16b572c437
Syaam Krishnaa S <krishgsmiley@gmail.com>
2016-10-05 23:39:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,6 +87,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/counter', function (req, res) {
+  counter =  counter + 1;
+  res.send(counter.toString());
+});
+
 app.get('/:articleName',function (req,res){
     // article Name == article -one
     // articles[articleName] == {} content object for artcle one
