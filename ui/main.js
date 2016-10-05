d42442ab21e398022675dc35ccc168d6cf3f5985
Syaam Krishnaa S <krishgsmiley@gmail.com>
2016-10-05 23:04:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,7 +10,7 @@ element.innerHTML = 'New value';
 
 // move the image
 
-var img = document.getElementById('img');
+var img = document.getElementById('madi');
 img.onclick = function (){
   img.style.marginleft = '100px';  
 };
