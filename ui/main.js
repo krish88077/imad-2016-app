722a79965e7831c975918df77fc21c3fd9f4a1be
Syaam Krishnaa S <krishgsmiley@gmail.com>
2016-10-14 13:58:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,28 +1,3 @@
-// console.log('Loaded!');
-
-
-// // change the text if the main-text div
-
-// var element = document.getElementById('main-text');
-
-// element.innerHTML = 'New value';
-
-
-// // move the image
-
-// var img = document.getElementById('madi');
-// var marginLeft = 0;
-// function moveRight(){
-//  marginLeft = marginLeft+1;
-//  img.style.marginLeft = marginLeft + 'px';
-// }
-
-// img.onclick = function (){
-//     var interval = setInterval(moveRight,50);
-// };
-
-//**************************************************************
-
 // counter code
 var button = document.getElementById('counter');
 
