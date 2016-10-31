8e6f6d0645cd62dbcd6032772452c09285b26aed
Syaam Krishnaa S <krishgsmiley@gmail.com>
2016-10-31 21:29:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,39 +15,6 @@ var app = express();
 app.use(morgan('combined'));
 
 
-var articles = {
-        'article-one' : {
-            title:  'Article one | Krish Smiley',
-            heading: 'Article one',
-            date: 'Sep 5, 2016',
-            content: `<p>
-                        This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
-                     </p>
-                     <p>
-                        This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
-                     </p>
-                     <p>
-                        This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
-                     </p>`
-        },
-        'article-two' : {
-            title:  'Article two | Krish Smiley',
-            heading: 'Article two',
-            date: 'Sep 10, 2016',
-            content: `<p>
-                        This is the content for my second article. 
-                     </p>`
-        },
-        'article-three' : {
-            title:  'Article three | Krish Smiley',
-            heading: 'Article three',
-            date: 'Sep 15, 2016',
-            content: `<p>
-                        This is the content for my third article. 
-                     </p>`
-        }
-};
-
 function createTemplate (data) {
     var title = data.title;
     var date = data.date;
