8530720b77403d35e3327c386baca08d881003b4
Syaam Krishnaa S <krishgsmiley@gmail.com>
2016-11-05 17:41:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 
 var config = {
-    user: 'coco98',
-    database: 'coco98',
+    user: 'krish88077',
+    database: 'krish88077',
     host: 'db.imad.hasura-app.io',
     port: '5432',
     password: process.env.DB_PASSWORD
@@ -19,58 +19,58 @@ app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(session({
     secret: 'someRandomSecretValue',
-    cookie: { maxAge: 1000 * 60 * 60 * 24 * 30}
+    cookie: {maxAge: 1000*60*60*24*30}
+    
 }));
+
 
 function createTemplate (data) {
     var title = data.title;
     var date = data.date;
     var heading = data.heading;
     var content = data.content;
-    
-    var htmlTemplate = `
+    var htmltemplate = `
     <html>
-      <head>
-          <title>
-              ${title}
-          </title>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link href="/ui/style.css" rel="stylesheet" />
-      </head> 
-      <body>
-          <div class="container">
-              <div>
-                  <a href="/">Home</a>
-              </div>
-              <hr/>
-              <h3>
-                  ${heading}
-              </h3>
-              <div>
-                  ${date.toDateString()}
-              </div>
-              <div>
-                ${content}
-              </div>
-              <hr/>
-              <h4>Comments</h4>
-              <div id="comment_form">
-              </div>
-              <div id="comments">
-                <center>Loading comments...</center>
-              </div>
-          </div>
-          <script type="text/javascript" src="/ui/article.js"></script>
-      </body>
+        <head>
+            <title>
+                ${title}
+            </title>
+            
+            <meta name = "viewport" content = "width=device-width, initial -scale=1"/>
+            <link href="/ui/style.css" rel="stylesheet" />
+            
+            
+        </head>
+        
+        <body>
+            <div class="container">
+                    <div>
+                        <a href="/">Home</a>
+                    </div>
+                    <hr/>
+                    <h3>
+                        ${heading}
+                    </h3>
+                    <div>
+                        ${date.toDateString()}
+                    </div>
+                    <div>
+                       ${content}
+                        
+                    </div>
+            </div>
+        </body>
+            
+            
+        
     </html>
     `;
-    return htmlTemplate;
+    return htmltemplate
 }
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
 
 function hash (input, salt) {
     // How do we create a hash?
@@ -154,81 +154,71 @@ app.get('/logout', function (req, res) {
    res.send('<html><body>Logged out!<br/><br/><a href="/">Back to home</a></body></html>');
 });
 
-var pool = new Pool(config);
-
-app.get('/get-articles', function (req, res) {
-   // make a select request
-   // return a response with the results
-   pool.query('SELECT * FROM article ORDER BY date DESC', function (err, result) {
-      if (err) {
-          res.status(500).send(err.toString());
-      } else {
-          res.send(JSON.stringify(result.rows));
-      }
-   });
-});
-
-app.get('/get-comments/:articleName', function (req, res) {
-   // make a select request
-   // return a response with the results
-   pool.query('SELECT comment.*, "user".username FROM article, comment, "user" WHERE article.title = $1 AND article.id = comment.article_id AND comment.user_id = "user".id ORDER BY comment.timestamp DESC', [req.params.articleName], function (err, result) {
-      if (err) {
-          res.status(500).send(err.toString());
-      } else {
-          res.send(JSON.stringify(result.rows));
-      }
-   });
-});
-
-app.post('/submit-comment/:articleName', function (req, res) {
-   // Check if the user is logged in
-    if (req.session && req.session.auth && req.session.auth.userId) {
-        // First check if the article exists and get the article-id
-        pool.query('SELECT * from article where title = $1', [req.params.articleName], function (err, result) {
-            if (err) {
-                res.status(500).send(err.toString());
-            } else {
-                if (result.rows.length === 0) {
-                    res.status(400).send('Article not found');
-                } else {
-                    var articleId = result.rows[0].id;
-                    // Now insert the right comment for this article
-                    pool.query(
-                        "INSERT INTO comment (comment, article_id, user_id) VALUES ($1, $2, $3)",
-                        [req.body.comment, articleId, req.session.auth.userId],
-                        function (err, result) {
-                            if (err) {
-                                res.status(500).send(err.toString());
-                            } else {
-                                res.status(200).send('Comment inserted!')
-                            }
-                        });
-                }
-            }
-       });     
-    } else {
-        res.status(403).send('Only logged in users can comment');
-    }
-});
-
-app.get('/articles/:articleName', function (req, res) {
-  // SELECT * FROM article WHERE title = '\'; DELETE WHERE a = \'asdf'
-  pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function (err, result) {
-    if (err) {
-        res.status(500).send(err.toString());
-    } else {
-        if (result.rows.length === 0) {
-            res.status(404).send('Article not found');
-        } else {
-            var articleData = result.rows[0];
-            res.send(createTemplate(articleData));
+var pool =  new Pool(config);
+app.get('/test-db', function (req,res) {
+    //make a request
+    // return response with results
+    pool.query('SELECT * FROM test', function(err,result){
+        if (err){
+            res.status(500).send(err.toString());
+        } else{
+            res.send(JSON.stringify(result.rows));
         }
-    }
-  });
+        
+    });
 });
 
-app.get('/ui/:fileName', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', req.params.fileName));
+
+var counter = 0;
+app.get('/counter', function (req, res) {
+  counter =  counter + 1;
+  res.send(counter.toString());
+});
+
+var names = [];
+app.get('/submit-name', function (req, res) { // URL:/ submit-name?name=xxxx
+    //get the name from the request
+    var name= req.query.name; //todo
+    names.push(name);
+    //JSON Javascript object notation
+    res.send(JSON.stringify(names));
+    
+});
+
+
+app.get('/articles/:articleName',function (req,res){
+    // article Name == article -one
+    // articles[articleName] == {} content object for artcle one
+
+    pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function(err,result) {
+        
+         if (err){
+            res.status(500).send(err.toString());
+        } else{
+            
+            if (result.rows.length === 0){
+                res.status(404).send('Article not found');
+            }else {
+                var articleData = result.rows[0];
+                res.send(createTemplate(articleData)); 
+            }
+        }
+        
+    });
+  
+});
+
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 
